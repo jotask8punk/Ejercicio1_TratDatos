9795e7899c42acf4b30fc71bb6d0370e0a3a4fde
jotask8punk <skaterpro01@hotmail.com>
2022-08-16 14:30:50 -0500

    push de prueba

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def get_price(ticker):
     header = {
         'User-Agent': 'Mozilla/5.0'
     }
+    """ comentario 1 """
 
     url = f"https://query1.finance.yahoo.com/v1/finance/search?q={ticker}&lang=en-US&region=US&quotesCount=6&newsCount=2&listsCount=2&enableFuzzyQuery=false&quotesQueryId=tss_match_phrase_query&multiQuoteQueryId=multi_quote_single_token_query&newsQueryId=news_cie_vespa&enableCb=true&enableNavLinks=true&enableEnhancedTrivialQuery=true&enableResearchReports=true&enableCulturalAssets=true&researchReportsCount=2"
     response = requests.get(url, headers=header)
